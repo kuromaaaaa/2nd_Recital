96217e3d9a924338e8b3b08c24eb3edc132e300a
kuromaaaaa <kuromaxa@gmail.com>
2023-09-05 19:53:46 +0900

    タイトルシーン切り替え前に走らせる

--- Assets/Script/LoadSceneScropt.cs
+++ Assets/Script/LoadSceneScropt.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class LoadSceneScropt : MonoBehaviour
 {
+    GameObject _runner;
+    Tweener _run;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _runner = GameObject.Find("Run1");
     }
 
     // Update is called once per frame
@@ -19,13 +22,13 @@ public class LoadSceneScropt : MonoBehaviour
 
     public void LoadScene(string name)
     {
-
+        Tweener _run = _runner.transform.DOMove(new Vector2(_runner.transform.position.x + 13, _runner.transform.position.y),1.5f).SetEase(Ease.Linear);
         StartCoroutine(nextScene(name));
-
     }
     IEnumerator nextScene(string N)
     {
-        yield return new WaitForSeconds(3);
+        _run.Kill();
+        yield return new WaitForSeconds(2);
         SceneManager.LoadScene(N);
     }
 }
