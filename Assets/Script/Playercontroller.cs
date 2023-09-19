b9504fac8f065533bd266740c747bf8af1b7498c
kuromaaaaa <kuromaxa@gmail.com>
2023-09-19 13:23:00 +0900

    WebGLでのビルドをした時のいくつかの設定

--- Assets/Script/Playercontroller.cs
+++ Assets/Script/Playercontroller.cs
@@ -1,10 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
-using TMPro.EditorUtilities;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Playercontroller : MonoBehaviour
 {
